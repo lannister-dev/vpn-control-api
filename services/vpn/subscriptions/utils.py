65d1cfe61203dda89762b222a178ea31d62cdae0
lannister <degterev907@gmail.com>
2026-03-20 15:54:31 +0300

    fix: replace dashes in subscription tokens, move rate limit after cache

--- services/vpn/subscriptions/utils.py
+++ services/vpn/subscriptions/utils.py
@@ -5,7 +5,7 @@ import secrets
 class SubscriptionUtils:
     @staticmethod
     def generate() -> str:
-        return secrets.token_urlsafe(32)
+        return secrets.token_urlsafe(32).replace("-", "_")
 
     @staticmethod
     def hash(token: str) -> str:
